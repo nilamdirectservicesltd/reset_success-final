c468b0e7f531f295ba55456d081837e8e2194aa9
nilamdirectservicesltd <125714818+nilamdirectservicesltd@users.noreply.github.com>
2025-06-08 15:35:04 +0530

    Delete main.js

--- main.js
+++
@@ -1,2 +0,0 @@
-// You can include any additional JavaScript code here if needed for the "reset_success.html" page.
-// For this example, there is no additional JavaScript code required for this page.
